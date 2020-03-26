f072b8a5e3164eb402bc71b8a879f6fbad29ae40
saadshamim95 <saadshamim95@gmail.com>
2020-03-26 14:41:27 +0530

    [Saad] Add. Adding DbContext, IRepository & RepositoryImpl in Repositry project

---
+++ Repository/IRepos/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.IRepos
+{
+    public interface IRepository
+    {
+        void AddEmployee(Employee employee);
+        void DeleteEmployee(int id);
+        void UpdateEmployee(Employee employee, int id);
+        IEnumerable<Employee> GetAllEmployees();
+        Employee GetEmployee(int id);
+    }
+}
---
+++ Repository/RepositoryImpl/RepositoryImpl.cs
@@ -0,0 +1,36 @@
+﻿using Model;
+using Repository.IRepos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.RepositoryImpl
+{
+    public class RepositoryImpl : IRepository
+    {
+        public void AddEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateEmployee(Employee employee, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
