86f91f69f5ddccfda61127c80867070c7a5ed251
saadshamim95 <saadshamim95@gmail.com>
2020-03-27 14:59:33 +0530

    [Saad] Add. Adding manager interface in the project

---
+++ Manager/Interface/IManager.cs
@@ -0,0 +1,17 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Interface
+{
+    public interface IManager
+    {
+        Task<int> AddEmployee(Employee employee);
+        Task<int> DeleteEmployee(int id);
+        Task<int> UpdateEmployee(Employee employee);
+        IEnumerable<Employee> GetAllEmployees();
+        Employee GetEmployee(int id);
+    }
+}
