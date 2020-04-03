9de1b24489f3143c0f2f629729c03cd4686471f2
saadshamim95 <saadshamim95@gmail.com>
2020-04-04 03:52:36 +0530

    [Saad] Add. Adding Celsius.cs in codebase

---
+++ QuantityMeasurement/Celsius.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Celsius.cs" company="BridgeLabz">
+//     Copyright © 2020 
+// </copyright>
+// <creator name="Saad Shamim"/>
+//-----------------------------------------------------------------------
+
+namespace QuantityMeasurement
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Class Celsius
+    /// </summary>
+    public class Celsius
+    {
+    }
+}
