bac6e42e1960f10ce2edc0031de3d3a0b80e2cc0
saadshamim95 <saadshamim95@gmail.com>
2020-04-04 02:45:32 +0530

    [Saad] Add. Adding LengthConversion.cs in codebase

---
+++ QuantityMeasurement/LengthConversion.cs
@@ -0,0 +1,19 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="LengthConversion.cs" company="BridgeLabz">
+//     Copyright © 2020 
+// </copyright>
+// <creator name="Saad Shamim"/>
+//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurement
+{
+    /// <summary>
+    /// Class for Converting Length
+    /// </summary>
+    public class LengthConversion
+    {
+    }
+}
