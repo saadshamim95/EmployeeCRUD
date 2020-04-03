ea0a4cc53e130c339d8e7e03399f076a28c2ab3a
saadshamim95 <saadshamim95@gmail.com>
2020-04-04 03:09:41 +0530

    [Saad] Add. Adding Kilogram.cs in codebase

---
+++ QuantityMeasurement/Kilogram.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Gram.cs" company="BridgeLabz">
+//     Copyright © 2020 
+// </copyright>
+// <creator name="Saad Shamim"/>
+//-----------------------------------------------------------------------
+
+namespace QuantityMeasurement
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Class Kilogram
+    /// </summary>
+    public class Kilogram
+    {
+    }
+}
