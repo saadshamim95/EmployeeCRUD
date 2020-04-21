c466c1a0fba2a5f067a06b660ca16538577ad2a0
saadshamim95 <saadshamim95@gmail.com>
2020-04-21 07:24:44 +0530

    [Saad] Add. Adding interface in Repository

---
+++ backend/Repository/Interface/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Interface
+{
+    public interface IRepository
+    {
+        decimal FeetToInch(Conversion value);
+        decimal InchToFeet(Conversion value);
+        decimal MeterToCentimeter(Conversion value);
+        decimal CentimeterToMeter(Conversion value);
+        decimal KilogramToGram(Conversion value);
+        decimal GramToKilogram(Conversion value);
+    }
+}
