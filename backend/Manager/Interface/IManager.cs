444a59171cedba448c70a2983a8c839fac5c401f
saadshamim95 <saadshamim95@gmail.com>
2020-04-21 07:31:58 +0530

    [Saad] Add. Adding Manager interface in codebase

---
+++ backend/Manager/Interface/IManager.cs
@@ -0,0 +1,17 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager.Interface
+{
+    public interface IManager
+    {
+        decimal FeetToInch(Conversion value);
+        decimal InchToFeet(Conversion value);
+        decimal MeterToCentimeter(Conversion value);
+        decimal CentimeterToMeter(Conversion value);
+        decimal KilogramToGram(Conversion value);
+        decimal GramToKilogram(Conversion value);
+    }
+}
