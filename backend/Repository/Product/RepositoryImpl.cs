b37c0c5d7552528bd6b1c5be3dfa2fa49cdb4cb8
saadshamim95 <saadshamim95@gmail.com>
2020-04-21 07:27:40 +0530

    [Saad] Add. Adding repository implementation in codebase

---
+++ backend/Repository/Product/RepositoryImpl.cs
@@ -0,0 +1,41 @@
+﻿using Model;
+using Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Product
+{
+    public class RepositoryImpl : IRepository
+    {
+        public decimal CentimeterToMeter(Conversion value)
+        {
+            return value.Centimeter / 100;
+        }
+
+        public decimal FeetToInch(Conversion value)
+        {
+            return value.Feet * 12;
+        }
+
+        public decimal GramToKilogram(Conversion value)
+        {
+            return value.Gram / 1000;
+        }
+
+        public decimal InchToFeet(Conversion value)
+        {
+            return value.Inch / 12;
+        }
+
+        public decimal KilogramToGram(Conversion value)
+        {
+            return value.KiloGram * 1000;
+        }
+
+        public decimal MeterToCentimeter(Conversion value)
+        {
+            return value.Meter * 100;
+        }
+    }
+}
