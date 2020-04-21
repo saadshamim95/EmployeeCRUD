85061a79ae8134e14f9c8638c878edc2064c0af1
saadshamim95 <saadshamim95@gmail.com>
2020-04-21 08:18:53 +0530

    [Saad] Refactor. Configuring Startup.cs in codebase

--- backend/QuantityMeasurementBackend/Startup.cs
+++ backend/QuantityMeasurementBackend/Startup.cs
@@ -2,19 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Manager.Interface;
+using Manager.Product;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Repository.Interface;
+using Repository.Product;
 
 namespace QuantityMeasurementBackend
 {
     public class Startup
     {
+        private IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
+            services.AddSingleton<IConfiguration>(this.configuration);
+            services.AddTransient<IRepository, RepositoryImpl>();
+            services.AddTransient<IManager, ManagerImpl>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "QuantiyMeasurement_Backend", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,12 +45,26 @@ namespace QuantityMeasurementBackend
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyApi V1");
+                });
+            }
+            else
+            {
+                app.UseHsts();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseHttpsRedirection();
+            app.UseCors(builder => {
+                builder.AllowAnyOrigin();
+                builder.AllowAnyHeader();
+                builder.AllowAnyMethod();
+            }
+                );
+            app.UseStaticFiles();
+            app.UseMvc();
         }
     }
 }
